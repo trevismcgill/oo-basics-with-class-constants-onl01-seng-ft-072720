51c6cd844d0478c66943458bee94bfa682ef530d
Trevis McGill <trevismcgill25@gmail.com>
2020-08-04 16:35:22 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,16 @@
 class Book
   attr_accessor :author, :page_count,
-  attr_reader :title
+  attr_reader :title, :genre
 
   GENRES = []
 
   def initialize(title)
     @title = title
+  end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES.push(genre)
   end
 
   def turn_page
